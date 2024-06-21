359d763630e0748a3f5007600e17c77d6b213356
Piyushk12 <88893547+Piyushk12@users.noreply.github.com>
2024-06-21 21:59:33 +0530

    basic project created

---
+++ app/src/main/java/com/example/passwormanager/home/data/PasswordDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.passwormanager.home.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.passwormanager.home.data.PasswordDao
+import com.example.passwormanager.home.data.PasswordEntity
+
+
+
+@Database(
+    entities = [PasswordEntity::class],
+    version = 3,
+    exportSchema = false
+
+)
+abstract class PasswordDatabase:RoomDatabase() {
+    abstract fun PasswordDao(): PasswordDao
+}
